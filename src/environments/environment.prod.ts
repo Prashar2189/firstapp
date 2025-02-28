0176764f009618c3018847b6b08e5eb4835cdd9c
Prashar2189 <raghav.prashar@appsimity.com>
2025-02-28 16:39:08 +0530

    new data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: "my-backend-5egq3apiu-rprashars-projects.vercel.app" // Change to your deployed backend URL
+    apiUrl: "my-backend-b15yvtofy-rprashars-projects.vercel.app" // Change to your deployed backend URL
   };
   
